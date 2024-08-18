6f88561fa2669a4df0b1010eb884270d984b1788
Siddhesh Rane <ruckerrane@gmail.com>
2024-08-18 11:15:18 -0400

    Added version control logic to WebAPI project

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using CodingChallangeWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApiVersioning(option =>
+            {
+                option.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0);
+                option.ReportApiVersions = true;
+            });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
 
 app.UseHttpsRedirection();
 
