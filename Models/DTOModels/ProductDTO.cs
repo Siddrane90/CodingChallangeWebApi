91d934ea52abc3a3b299cc386596b29c9f3bc24f
Siddhesh Rane <ruckerrane@gmail.com>
2024-08-21 01:16:17 -0400

    Product DTO to provide related information.

---
+++ Models/DTOModels/ProductDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CodingChallangeWebApi.Models.DTOModels
+{
+    public class ProductDTO
+    {
+        public string ProductName { get; set; }
+        public decimal PricePerUnit { get; set; }
+
+        public int OrderQuantity { get; set; }
+    }
+}
