65732a1abdd82316e49c5b31c892133e643e5bef
Siddhesh Rane <ruckerrane@gmail.com>
2024-08-21 01:00:50 -0400

    Payment Provider Entity  class to retrived Payment provider information.

---
+++ Models/DataEntityModels/PaymentProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingChallangeWebApi.Models.DataEntityModels;
+
+public partial class PaymentProvider
+{
+    public int Id { get; set; }
+
+    public string? ProviderName { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? Apiurl { get; set; }
+
+    public string? ApiKeyEncrypted { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+    public virtual ICollection<PaymentTypeAndRule> PaymentTypeAndRules { get; set; } = new List<PaymentTypeAndRule>();
+}
