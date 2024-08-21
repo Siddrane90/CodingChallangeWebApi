a6f4c32315e71ae7d25eef1cbf8949ec51457844
Siddhesh Rane <ruckerrane@gmail.com>
2024-08-21 00:54:52 -0400

    Automapped implemented to allow auto mapping of Entity Objects and DTO objects.

---
+++ Models/AutoMapper/OrderMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CodingChallangeWebApi.Models.DataEntityModels;
+using CodingChallangeWebApi.Models.DTOModels;
+namespace CodingChallangeWebApi.Models.OrderMappingProfile
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile()
+        {
+            CreateMap<OrderDTO, Order>();
+        }
+    }
+}
