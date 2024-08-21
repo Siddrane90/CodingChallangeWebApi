1852c9669612c7a1a594f64f8eaeeff71b951517
Siddhesh Rane <ruckerrane@gmail.com>
2024-08-21 00:34:42 -0400

    Added Dependency Injection for services , Cors Policy & API Versioning .

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
 using CodingChallangeWebApi.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var allowSpecificOriginsPolicy = "allowSpecificOriginsPolicy";
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowSpecificOriginsPolicy,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,7 +27,11 @@ builder.Services.AddApiVersioning(option =>
                 option.DefaultApiVersion = new Asp.Versioning.ApiVersion(1, 0);
                 option.ReportApiVersions = true;
             });
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+
 var app = builder.Build();
+app.UseCors(allowSpecificOriginsPolicy);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
