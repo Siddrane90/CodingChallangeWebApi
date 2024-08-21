a2ff88a42f54377b9a768fb6cbf6e4a203ba2f35
Siddhesh Rane <ruckerrane@gmail.com>
2024-08-21 00:52:20 -0400

    API Response template created to provide a standard response for API.

---
+++ DataAccess/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace CodingChallangeWebApi.DataAccess
+{
+    public class ApiResponse<T>
+    {
+        public bool StatusSucess { get; set; }
+        public T Data { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public ApiResponse()
+        {
+            StatusSucess = true;
+        }
+    }
+}
