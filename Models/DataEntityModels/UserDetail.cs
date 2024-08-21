ae40068d352f9b45694f974b83759d8568f85fe5
Siddhesh Rane <ruckerrane@gmail.com>
2024-08-21 01:05:49 -0400

    Entity class for User details for stroring and retriveing User information details.

---
+++ Models/DataEntityModels/UserDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingChallangeWebApi.Models.DataEntityModels;
+
+public partial class UserDetail
+{
+    public int Id { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? FullName { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
